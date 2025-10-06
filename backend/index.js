81d3a1b1b5fa69bcc21a0cd411d89e1d113d059a
Aayush Mathur <aayushmathur@microsoft.com>
2025-10-06 16:31:45 +0530

    Trigger deployment after restructuring
    - Small comment update to trigger GitHub Actions workflow
    - Test if backend deployment works with new ./backend directory structure
    - Functions are working locally, need to redeploy to Azure

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 // Azure Functions App Entry Point
 // This file imports and registers all function modules
+// Updated to trigger redeployment after restructuring
 
 // Import all function modules
 require('./demoFunctions');
